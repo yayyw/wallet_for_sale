eb6c99fbafba18693d0af4c8fde6386001f14d5f
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-20 16:09:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,6 +83,9 @@ for i in range(number):
             st.session_state["cart"].append(wallet_details)
             st.success("Wallet {} added to cart!".format(i+1)) #add to cart
 
+        if remove not in st.session_state:
+            st.session_state[remove] = False
+            
         if st.button("remove", key="remove_cart" + str(i)) and len(list_of_wallet_prices) > i:
             wallet_price = list_of_wallet_prices[i]
             st.session_state["total_price"] = max(0, st.session_state["total_price"] - wallet_price)
@@ -91,7 +94,7 @@ for i in range(number):
             st.success(f"Wallet {i+1} removed from cart :(") #i starts from 0
 
         elif len(st.session_state["cart"]) == 0:
-            st.session_state[remove] = False
+            st.write("No wallets in cart:(")
 
 
 #Discounts
